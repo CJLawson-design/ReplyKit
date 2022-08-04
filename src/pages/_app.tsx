715b9a5ff1d84657cfdbc791c7116655624b502a
CJLawson-design <lawsoncj3@gmail.com>
2022-08-03 22:09:47 -0500

    updated firebase config & auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
+import { ProvideAuth } from "../lib/auth";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ProvideAuth>
+      <Component {...pageProps} />
+    </ProvideAuth>
+  );
 };
 
 export default MyApp;
