7b4b7769c6277e5547e59f40bbb6ca47b09c5922
CJLawson-design <lawsoncj3@gmail.com>
2022-08-04 16:07:42 -0500

    refactor: added handle function to handle incoming  raw user json data

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -17,47 +17,64 @@ export const useAuth = () => {
 
 function useAuthProvider() {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   // testing
   console.log(user);
 
+  // handle raw user json data
+  const handleUser = (rawUser) => {
+    if (rawUser) {
+      const user = formatUser(rawUser);
+      setLoading(false);
+      setUser(user);
+      return user;
+    } else {
+      setLoading(false);
+      setUser(false);
+      return false;
+    }
+  };
+
   // Sign in with Github
   const signinWithGithub = () => {
+    setLoading(true);
     const provider = new firebase.auth.GithubAuthProvider();
 
     return firebase
       .auth()
       .signInWithPopup(provider)
-      .then((response) => {
-        setUser(response.user);
-        return response.user;
-      });
+      .then((response) => handleUser(response.user));
   };
+
   // Signout
   const signout = () => {
     return firebase
       .auth()
       .signOut()
-      .then(() => {
-        setUser(false);
-      });
+      .then(() => handleUser(false));
   };
 
   useEffect(() => {
-    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        setUser(user);
-      } else {
-        setUser(false);
-      }
-    });
-
+    const unsubscribe = firebase.auth().onAuthStateChanged(handleUser);
     return () => unsubscribe();
   }, []);
 
   return {
     user,
+    loading,
     signinWithGithub,
     signout,
   };
 }
+
+// Format user json data when handleUser() function is hit
+const formatUser = (user) => {
+  return {
+    uid: user.uid,
+    email: user.email,
+    name: user.displayName,
+    provider: user.providerData[0].providerId,
+    photoUrl: user.photoURL,
+  };
+};
