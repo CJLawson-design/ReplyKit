9bc1d5395b838aff7faefd59b22a9c94ca161c36
CJLawson-design <lawsoncj3@gmail.com>
2022-08-10 00:58:04 -0500

    updated tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
